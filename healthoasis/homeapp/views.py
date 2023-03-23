256c4665a748a1a9c4520bfbb5540325ef3b1f51
Paul Latham <pl00600@surrey.ac.uk>
2023-03-23 12:14:40 +0000

    Add: Basic Weather API Functionality

--- healthoasis/homeapp/views.py
+++ healthoasis/homeapp/views.py
@@ -5,12 +5,17 @@ from django.urls import reverse_lazy
 
 from .models import *
 from .forms import *
+
+import requests
 # Create your views here.
 
 
 #View for homepage
 def home(request):
-    context = {}
+    response = requests.get('http://api.weatherapi.com/v1/current.json?key=59208b37dbca49ae860121639231003&q=London&aqi=no')
+    weather = response.json()
+    context = {'location':(weather['location']['name'] + ", " + weather['location']['country']),
+               'condition':weather['current']['condition']['text']}
     return render(request, 'homeapp/home.html', context)
 
 #View to register a user
